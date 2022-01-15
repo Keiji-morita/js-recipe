f948061a0019de2e1c67eda2a4a9247e3020efb3
Keiji-morita <keijim3116@gmail.com>
2022-01-15 22:21:42 +0900

    発展の課題を解きました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,45 @@
+const genkifunction = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      console.log(i + "!!!!!")
+    } else {
+      console.log(i)
+    }
+  }
+}
 
-const genkifunction = function(number){
-for (let i = 1; i <= number; i++) {
-  if (i % 3 === 0) {
-    console.log("!!!!!")
-  } else {
-    console.log(i)
+genkifunction(100)
+
+//発展 ★ - FizzBuzz
+const fizzbuzz = function (numF) {
+  for (let n = 1; n <= numF; n++) {
+    if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+fizzbuzz(30)
+
+//発展 ★★ - ３が大好き
+const lovethree = function (num3) {
+  for (let g = 1; g <= num3; g++) {
+   for(let h = 3; h <= num3; h = h + 10){
+    if (g % 10 === 3) {
+      console.log(g +"!!!!!")
+    } else if(g / h === 10){
+      console.log(g + "!!!!!")
+    } else {
+      console.log(g)
+    }
   }
  }
 }
 
-genkifunction(100);
+ lovethree(400)
+
+
