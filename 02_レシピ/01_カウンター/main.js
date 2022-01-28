d37f39499e8518fb208b531aaac5737772c43811
Keiji-morita <keijim3116@gmail.com>
2022-01-28 20:12:03 +0900

    発展 ★ - 便利なボタン

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const negativeButton = document.getElementById("negative-button")
+const multipleButton = document.getElementById("multiple-button")
 
 let count = 0
 
@@ -8,5 +10,15 @@ plusButton.onclick = function () {
 
   display.textContent = count
 }
-
 //発展 ★ - 便利なボタン
+negativeButton.onclick = function () {
+  count -= 1
+
+  display.textContent = count
+}
+
+multipleButton.onclick = function () {
+  count *= 2
+
+  display.textContent = count
+}
